d56e542534b5fd9c45f7ba8f8abe480b982816d0
aryachawan <aryachawan05@gmail.com>
2025-12-18 18:07:35 +0530

    top navbar completed

--- components/top-navbar.js
+++ components/top-navbar.js
@@ -1,24 +1,18 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function TopNavbar(){
-    
-  return(
+    const pathname = usePathname();
+
+    const basedesign = "px-5 py-3 border border-white rounded-3xl text-lg transition-all duration-300 ease-in-out"
+    const activelink = "bg-white text-black"
+    const inactivelink = "hover:bg-white hover:text-black"
+    return(
     <div className="w-full p-4 flex items-center justify-center gap-4">
-      <Link
-        href="/"
-        className="px-5 py-3 border border-white rounded-3xl text-lg
-                   transition-all duration-500 ease-in-out
-                   hover:bg-white hover:text-black"
-      >
+      <Link href="/" className={`${basedesign} ${pathname === "/" ? activelink : inactivelink}`}>
         Overview
       </Link>
-
-      <Link
-        href="/contact-me"
-        className="px-5 py-3 border border-white rounded-3xl text-lg
-                   transition-all duration-500 ease-in-out
-                   hover:bg-white hover:text-black"
-      >
+      <Link href="/contact-me" className={`${basedesign} ${pathname === "/contact-me" ? activelink : inactivelink}`}>
         Contact me
       </Link>
     </div>
